abe52a96ba700821dd28cd1fe898fac539e50a6e
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-12-13 12:26:47 -0500

    Update and rename cm.mk to ose.mk

--- ose.mk
+++ ose.mk
@@ -2,23 +2,21 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OSE stuff.
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d801/d801.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d801
-PRODUCT_NAME := cm_d801
+PRODUCT_NAME := ose_d801
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D801
 PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_tmo_us/g2:4.4.2/KOT49I.D80120e/D80120e.1401720405:user/release-keys
 PRIVATE_BUILD_DESC="g2_tmo_us-user 4.4.2 KOT49I D80120e.1401720405 release-keys"
-
-PRODUCT_PACKAGES += Torch
